a4789e29b99791f6d4e7ff33025934b99c6ac0f4
Maxime Buteau <maxbut@live.ca>
2022-01-16 14:16:49 -0500

    Added optimus manager icon in systray

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -45,6 +45,7 @@ run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
 blueberry-tray &
+optimus-manager-qt &
 picom --config $HOME/.config/qtile/scripts/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
