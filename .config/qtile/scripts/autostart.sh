fae20e5093b15e0e9144cbe4d57d8ddd50779e48
Maxime Buteau <maxbut@live.ca>
2022-01-22 12:52:29 -0500

    Megasync starts on boot

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -27,7 +27,8 @@ nrScreens=${#screens[@]}
 # TODO: resolution should not be hardcoded, as I sometimes connect to a different resolution monitor
 if [ $nrScreens -eq 2 ]
 then
-    xrandr --output ${screens[0]} --mode 1920x1080 --pos 2560x0 --rotate normal --output ${screens[1]} --primary --mode 2560x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+    #xrandr --output ${screens[0]} --mode 1920x1080 --pos 2560x0 --rotate normal --output ${screens[1]} --primary --mode 2560x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+    xrandr --output ${screens[0]} --mode 1920x1080 --pos 1920x0 --rotate normal --output ${screens[1]} --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 fi
 
 # screen layout for 2560 dual monitor
@@ -56,8 +57,9 @@ run nm-applet &
 run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
+megasync &
 blueberry-tray &
-optimus-manager-qt &
+#optimus-manager-qt &
 picom --config $HOME/.config/qtile/scripts/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
