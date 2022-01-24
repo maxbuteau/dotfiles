6beedb4d3177ff7269c88804b5edfd5bc6f78723
Maxime Buteau <maxbut@live.ca>
2022-01-24 12:21:07 -0500

    Added alacritty config

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -27,8 +27,8 @@ nrScreens=${#screens[@]}
 # TODO: resolution should not be hardcoded, as I sometimes connect to a different resolution monitor
 if [ $nrScreens -eq 2 ]
 then
-    #xrandr --output ${screens[0]} --mode 1920x1080 --pos 2560x0 --rotate normal --output ${screens[1]} --primary --mode 2560x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-    xrandr --output ${screens[0]} --mode 1920x1080 --pos 1920x0 --rotate normal --output ${screens[1]} --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+    xrandr --output ${screens[0]} --mode 1920x1080 --pos 2560x0 --rotate normal --output ${screens[1]} --primary --mode 2560x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+    #xrandr --output ${screens[0]} --mode 1920x1080 --pos 1920x0 --rotate normal --output ${screens[1]} --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 fi
 
 # screen layout for 2560 dual monitor
