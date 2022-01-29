81ea27fc69e22bffc2dd45dc411c4e14b092d1a7
Maxime Buteau <maxbut@live.ca>
2022-01-28 22:32:53 -0500

    Windows more opaque

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -473,8 +473,8 @@ widgets_screen2 = init_widgets_screen2()
 
 
 def init_screens():
-    return [Screen(top=bar.Bar(widgets=init_widgets_screen1(), size=26, opacity=0.8)),
-            Screen(top=bar.Bar(widgets=init_widgets_screen2(), size=26, opacity=0.8))]
+    return [Screen(top=bar.Bar(widgets=init_widgets_screen1(), size=26, opacity=0.9)),
+            Screen(top=bar.Bar(widgets=init_widgets_screen2(), size=26, opacity=0.9))]
 screens = init_screens()
 
 
